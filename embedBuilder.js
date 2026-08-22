c821fdaa6a796a7827882404633fd2d1910b72e6
heyba481-hue <heyba481@gmail.com>
2026-08-21 21:47:53 -0300

    Create embedBuilder.js

---
+++ embedBuilder.js
@@ -0,0 +1,16 @@
+const { SlashCommandBuilder, PermissionFlagsBits, MessageFlags } = require('discord.js');
+const embedSession = require('../../utils/embedSession');
+const { construirPainel } = require('../../builders/embedPreview');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('embed-builder')
+    .setDescription('Abre o painel avançado de criação de Embed / Components V2.')
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
+  async execute(interaction) {
+    const sessao = embedSession.obter(interaction.user.id, interaction.guildId, interaction.channelId);
+    const painel = construirPainel(sessao);
+    await interaction.reply({ ...painel, flags: MessageFlags.Ephemeral });
+  },
+};
+
