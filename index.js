4b98936f0fff80209b23f8935f1eec0375f883fb
heyba481-hue <heyba481@gmail.com>
2026-08-23 14:33:30 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-const config = require('./config');
-const { criarClient } = require('./client');
+const config = require('./src/config');
+const { criarClient } = require('./src/client');
+const { registrarComandos } = require('./src/deploy-commands');
 
-const client = criarClient();
+async function iniciar() {
+  try {
+    await registrarComandos();
+  } catch (err) {
+    console.error('[index] Falha ao registrar comandos automaticamente (o bot vai subir mesmo assim):', err.message);
+  }
 
-client.login(config.token).catch((err) => {
-  console.error('[index] Falha ao logar no Discord. Verifique se DISCORD_TOKEN está correto.', err);
-  process.exit(1);
-});
+  const client = criarClient();
+  await client.login(config.token).catch((err) => {
+    console.error('[index] Falha ao logar no Discord. Verifique se DISCORD_TOKEN está correto.', err);
+    process.exit(1);
+  });
+}
+
+iniciar();
 
 process.on('unhandledRejection', (err) => {
   console.error('[index] Rejeição de Promise não tratada:', err);
